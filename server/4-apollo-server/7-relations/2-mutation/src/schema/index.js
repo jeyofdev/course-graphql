d81ca25e2cdeb200e8f9482c42898f876af20391
jeyofdev <jgregoire.pro@gmail.com>
2022-03-03 14:55:24 +0100

    apollo-server : create mutation with relation

---
+++ server/4-apollo-server/7-relations/2-mutation/src/schema/index.js
@@ -0,0 +1,30 @@
+import { gql } from 'apollo-server';
+
+// Set schema
+const schema = gql(`
+    type User {
+        id: ID
+        name: String
+        email: String
+        posts: [Post]
+    }
+
+    type Post {
+        id: ID
+        title: String
+        content: String
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        posts: [Post]
+    }
+
+    type Mutation {
+        addUser(name: String, email: String): User
+        addPost(title: String, content: String, userId: ID): Post
+    }
+`);
+
+export default schema;
