8749d5966b88ca320861c1e8d02f4b9da564bb5b
jeyofdev <jgregoire.pro@gmail.com>
2022-03-04 10:15:13 +0100

    apollo-client-react : create basic query

---
+++ client/1-react/2-query/1-base/src/App.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from '@apollo/client';
+import { GET_BOOKS } from './queries';
+
+const App = () => {
+    const { loading, error, data } = useQuery(GET_BOOKS);
+    let displayBooks;
+
+    if (loading) {
+        displayBooks = <p>Loading...</p>;
+    }
+
+    if (error) {
+        displayBooks = <p>Error 😔</p>;
+    }
+
+    if (data) {
+        displayBooks = (
+            <div id="book-list">
+                {data?.books.map((book) => (
+                    <li key={book.id}>{book.name}</li>
+                ))}
+            </div>
+        );
+    }
+
+    return (
+        <div id="main" className="App">
+            <h1>Book's List</h1>
+
+            {displayBooks}
+        </div>
+    );
+};
+
+export default App;
